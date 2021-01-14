6756804ed696f97f4806ef1f5e9ca25187218c82
Mariano Acevedo <marianoezequielacevedo@hotmail.com>
2021-01-14 20:07:54 -0300

    MercadoLiebre html

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.listen(3000);
+
+const staticFolder = path.resolve(__dirname, "./public");
+app.use(express.static(staticFolder));
+
+app.get('/',(req,res) => {
+    res.sendFile(path.resolve(__dirname,'./views/home.html'))
+});
+
